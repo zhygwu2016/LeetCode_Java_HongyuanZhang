82db61786703faffedf55e5a6ddb98ea55ccab49
Hongyuan Zhang <zhygwu2016@gmail.com>
2020-05-08 00:11:28 -0700

    20200507

--- src/Online_Assessment/Minimum_Distance.java
+++ src/Online_Assessment/Minimum_Distance.java
@@ -1,4 +1,63 @@
 package Online_Assessment;
+/*
+The current selected programming language is Java. We emphasize the
+submission of a fully working code over partially correct but efficient code.
+Once submitted, you cannot review this problem again. You can
+use System.out.print/n() to debug your code. The System.out.print/n() may not
+work in case of syntax/runtime error. The version of 'DK being used is 1.8.
+Amazon Fresh is a grocery delivery service that offers consumers the option of
+purchasing their groceries online and having them delivered on schedule. The
+Amazon Fresh team is planning a route for a delivery truck to deliver customer orders
+in the city of Techlandia. The planner will create a delivery area for each order to
+effectively plan the route. The area is abstracted as a grid. Not all locations are
+accessible by road. The truck only needs to make a single delivery.
+Write an algorithm to determine the minimum distance required for the truck to
+deliver the order.
+
+Assumptions:
+Some places in the delivery area cannot be accessed by the driver, as there are no
+roads in those locations.
+The delivery area can be represented as a two-dimensional grid of integers, where each
+integer represents one cell.
+The truck must start from the top-left corner of the area, which is always accessible
+and can move one cell up, down, left, or right at a time.
+The truck must navigate around the areas without roads and cannot leave the area.
+The accessible areas are represented as 1, areas without roads are represented by O and
+the order destination is represented by 9.
+
+Input
+The input to the function/method consists of three arguments:
+numRows, an integer representing the number of rows;
+numColumns, an integer representing the number of columns;
+area, representing the two-dimensional grid of integers.
+
+Output
+Return an integer representing the total distance traversed to deliver the order else
+return-1.
+
+Constraints
+1 <= numRows, numColumns <= 10^3
+
+Example
+Input:
+numRows = 3
+numColumns = 3
+
+area =
+[[1, 0, 0],
+ [1, 0, 0],
+ [1, 9, 1]]
+
+Output:
+3
+
+Explanation:
+Starting from the top-left corner, the truck traversed the cells (0,0) -> (1,0) -> (2,0)->(2,1).
+The truck traversed the total distance
+to deliver the order.
+So, the output is 3.
+
+ */
 
 // IMPORT LIBRARY PACKAGES NEEDED BY YOUR PROGRAM
 // SOME CLASSES WITHIN A PACKAGE MAY BE RESTRICTED
